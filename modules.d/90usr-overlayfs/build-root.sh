15aef9ccfeaecf4a7f51d24c87a3af3710776d7b
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2024-12-17 11:01:51 -0500

    all together, not sure if it works with /usr

--- modules.d/90usr-overlayfs/build-root.sh
+++ modules.d/90usr-overlayfs/build-root.sh
@@ -8,8 +8,9 @@ NEWROOT=${NEWROOT:-'/sysroot'}
 ROOT=$(lsblk -o NAME,TYPE,PARTTYPE --json | jq -r '.blockdevices[] | select(.type == "disk") | .children[] | select(.parttype == "4f68bce3-e8cd-4db1-96e7-fbcaf984b709")')
 echo "ROOT $ROOT" > /output.txt
 
-DNAME=$(lsblk -o NAME,TYPE --json | jq -r '.blockdevices[] | select(.type == "disk") | .name ')
-echo "DNAME $DNAME" >> /output.txt
+# DNAME=$(lsblk -o NAME,TYPE --json | jq -r '.blockdevices[] | select(.type == "disk") | .name ')
+# echo "DNAME $DNAME" >> /output.txt
+DNAME=vda
 
 # find /usr
 # eval $(lsblk -P -o NAME,TYPE,PARTTYPE | grep 8484680c-9521-48c6-9c11-b0720656f69e)
@@ -22,11 +23,13 @@ UNAME=vda3
 
 if ! [ -z "${ROOT:-}" ]; then
 	echo "ROOT EXISTS" >> /output.txt
-	mount /dev/$UNAME $NEWROOT/usr
+	# mount /dev/$UNAME $NEWROOT/usr
 	exit 0
 fi
 
 echo "NO ROOT" >> /output.txt
+
+echo "" > /new_root
 
 #### root (encrypted) in initramfs
 mkdir /etc/repart.d
@@ -37,20 +40,21 @@ Encrypt=tpm2" > /etc/repart.d/encr.conf
 #FactoryReset=yes
 
 systemd-repart /dev/$DNAME --dry-run=no --no-pager --definitions=/etc/repart.d --tpm2-device=auto --tpm2-pcrs=7 # --factory-reset=yes
-sleep 5
+# sleep 5
 # TODO: this is only for writable /usr
-mount /dev/mapper/root $NEWROOT
+# mount /dev/mapper/root $NEWROOT
+# mount -o remount,rw $NEWROOT
 ######
-mkdir -p $NEWROOT/usr
-mkdir -p $NEWROOT/etc
+# mkdir -p $NEWROOT/usr
+# mkdir -p $NEWROOT/etc
 
 # TODO: this is only for writable /usr
-mount /dev/$UNAME $NEWROOT/usr
+# mount /dev/$UNAME $NEWROOT/usr
 ######
-systemd-sysusers --root $NEWROOT
-systemd-tmpfiles --root $NEWROOT --create
+# systemd-sysusers --root $NEWROOT
+# systemd-tmpfiles --root $NEWROOT --create
 
-echo -n "-F " > $NEWROOT/.autorelabel
+# echo -n "-F " > $NEWROOT/.autorelabel
 # $NEWROOT/usr/bin/chcon system_u:object_r:root_t:s0 $NEWROOT/*
 # mkdir $NEWROOT/proc $NEWROOT/dev $NEWROOT/sys
 # mkdir $NEWROOT/var/log/audit
--- modules.d/90usr-overlayfs/module-setup.sh
+++ modules.d/90usr-overlayfs/module-setup.sh
@@ -10,14 +10,23 @@ depends() {
 }
 
 install() {
-    inst_hook pre-mount 99 "$moddir/build-root.sh"
+    # inst_hook pre-mount 99 "$moddir/build-root.sh"
+
+    inst_simple "$moddir/build-root.service" "$systemdsystemunitdir/build-root.service"
+    inst_simple "$moddir/build-root.sh" "/usr/local/bin/build-root.sh"
+    $SYSTEMCTL -q --root "$initdir" enable build-root.service
+
+
+    inst_simple "$moddir/populate-root.service" "$systemdsystemunitdir/populate-root.service"
+    inst_simple "$moddir/populate-root.sh" "/usr/local/bin/populate-root.sh"
+    $SYSTEMCTL -q --root "$initdir" enable populate-root.service
+
 
     inst_simple "$moddir/overlay-usr.service" "$systemdsystemunitdir/overlay-usr.service"
-    inst_simple "$moddir/setup-overlay-usr.sh" "/usr/local/bin/setup-overlay-usr.sh"
-
+    inst_simple "$moddir/overlay-usr.sh" "/usr/local/bin/overlay-usr.sh"
     $SYSTEMCTL -q --root "$initdir" enable overlay-usr.service
 
-    inst_multiple -o mkfs.btrfs mkfs.ext4 mkfs.xfs lsblk jq
+    inst_multiple -o mkfs.btrfs mkfs.ext4 mkfs.xfs lsblk jq restorecon
 }
 
 
