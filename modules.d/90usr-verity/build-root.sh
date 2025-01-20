4547d537d0b61feb4364b72811b466e985c04fcf
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2025-01-20 09:16:34 -0500

    cleanup

--- modules.d/90usr-verity/build-root.sh
+++ modules.d/90usr-verity/build-root.sh
@@ -7,7 +7,6 @@ echo "ROOT $ROOT" > /output.txt
 
 DNAME=$(lsblk -o NAME,TYPE --json | jq -r '.blockdevices[] | select(.type == "disk") | .name ')
 echo "DNAME $DNAME" >> /output.txt
-# DNAME=vda
 
 if ! [ -z "${ROOT:-}" ]; then
 	echo "ROOT EXISTS" >> /output.txt
@@ -19,7 +18,6 @@ echo "NO ROOT" >> /output.txt
 
 echo "" > /new_root
 
-#### root (encrypted) in initramfs
 mkdir /etc/repart.d
 echo -n "[Partition]
 Type=root
@@ -28,21 +26,11 @@ Encrypt=tpm2" > /etc/repart.d/encr.conf
 
 # REPART_OUT=$(systemd-repart /dev/$DNAME --dry-run=no --no-pager --definitions=etc/repart.d --tpm2-device=auto --tpm2-pcrs=7 --json pretty | jq -r '.[] | select(.type == "root-x86-64") | .activity')
 systemd-repart /dev/$DNAME --dry-run=no --no-pager --definitions=/etc/repart.d --tpm2-device=auto --tpm2-pcrs=7 # --factory-reset=yes
-# sleep 5
+
 # TODO: this is only for writable /usr
 # mount /dev/mapper/root $NEWROOT
-# mount -o remount,rw $NEWROOT
 ######
-# mkdir -p $NEWROOT/usr
-# mkdir -p $NEWROOT/etc
 
 # TODO: this is only for writable /usr
 # mount /dev/$UNAME $NEWROOT/usr
 ######
-# systemd-sysusers --root $NEWROOT
-# systemd-tmpfiles --root $NEWROOT --create
-
-# echo -n "-F " > $NEWROOT/.autorelabel
-# $NEWROOT/usr/bin/chcon system_u:object_r:root_t:s0 $NEWROOT/*
-# mkdir $NEWROOT/proc $NEWROOT/dev $NEWROOT/sys
-# mkdir $NEWROOT/var/log/audit
